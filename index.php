aa9d005417f6b646857208426749c28dca6ca7ea
Junaid Bashir <junaid@crayondevelopers.com>
2024-06-23 00:53:14 +0100

    init

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<?php 
+    $name = "wendoge";
+    ?>
+<head>
+    <meta charset="utf-8">
+    <title>wendoge</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/css//style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>

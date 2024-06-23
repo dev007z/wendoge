063182a4fe4feac75bb4514f4ef51fd9e46d2c44
Junaid Bashir <junaid@crayondevelopers.com>
2024-06-23 01:02:32 +0100

    add logo, favicon to logo add font file

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
-<?php 
-    $name = "wendoge";
-    ?>
+<?php
+$name = "wendoge";
+?>
+
 <head>
     <meta charset="utf-8">
     <title>wendoge</title>
@@ -10,10 +11,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/css//style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="./assets/images/logo.png">
 </head>
 
 <body>
-    
+    <header>
+        <h1 class="logo-text">wendoge</h1>
+    </header>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 

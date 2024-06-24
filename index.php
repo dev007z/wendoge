5198d74e475ee07c7829bb75b07769bb8f9dc770
Junaid Bashir <junaid@crayondevelopers.com>
2024-06-24 02:24:13 +0100

    socials and buy button done

--- index.php
+++ index.php
@@ -17,32 +17,58 @@ $name = "wendoge";
 
 <body>
     <header class="text-center">
-        <div class="header-body row text-center">
+        <div class="header-body text-center">
             <div class="top">
                 <h1 class="logo-text ">wendoge</h1>
                 <p class="text-uppercase framer-text fs-3">Powered by Solana</p>
             </div>
-            <div class="col-1"></div>
-            <div class="col-5" style="padding-bottom: -70px;">
-                <img src="./assets/images/logo.png" alt="logo" height="519" width="519">
-            </div>
-            <div class="col-5 framer-text1" style="align-self: center;">
-                <p class="text-white fs-5">wendoge is <span>the bridge</span></p>
-                <p class="text-white fs-3">between the canine and feline world</p>
-                <p class="text-white fs-5">on the <span>Solana network</span></p>
-                <div class="row pt-3">
-                    <div class="col-3"></div>
-                    <div class="col-3">
-                        <div class="button-shake text-center fs-5">
-                            Buy wendoge
-                        </div>
-                    </div>
-                    <div class="col-3"></div>
-                    <div class="col-3"></div>
+            <div class="row">
+                <div class="col-xl-1"></div>
+                <div class="col-xl-5" style="padding-bottom: -70px;">
+                    <img src="./assets/images/logo.png" alt="logo" height="519" width="519">
                 </div>
-
+                <div class="col-xl-5 framer-text1 pt-3" style="align-self: center;">
+                    <p class="text-white fs-5">wendoge is <span>the bridge</span></p>
+                    <p class="text-white fs-3">between the canine and feline world</p>
+                    <p class="text-white fs-5">on the <span>Solana network</span></p>
+                    <div class="row pt-3">
+                        <div class="col-3"></div>
+                        <div class="col-3" style="margin-right: 20px;">
+                            <div class="button-shake text-center fs-5">
+                                Buy wendoge
+                            </div>
+                        </div>
+                        <div class="col-3" style="margin-left: 20px;">
+                            <div class="row socials">
+                                <div class="col button-shake">
+                                    <a href="#">
+                                        <img src="./assets/images/twitter.png" alt="" height="30px">
+                                    </a>
+                                </div>
+                                <div class="col button-shake">
+                                    <a href="#">
+                                        <img src="./assets/images/telegram.png" alt="" height="30px">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="row socials">
+                                <div class="col button-shake">
+                                    <a href="#">
+                                        <img src="./assets/images/raydium.png" alt="" height="35px">
+                                    </a>
+                                </div>
+                                <div class="col button-shake">
+                                    <a href="#">
+                                        <img src="./assets/images/dexscreener.png" alt="" height="35px">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3"></div>
+                    </div>
+                </div>
+                <div class="col-xl-1"></div>
             </div>
-            <div class="col-1"></div>
         </div>
     </header>
     <section id="tokenomics">
